7913332d57a86893ee62065605f71d063c28b0c9
Marcos Rios <marcosrioos@gmail.com>
2022-07-28 19:57:57 -0300

    trying to fix images render

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,7 +12,7 @@ export default function Card({ name, image, types }) {
         {types.map((e, index) => {
           return (
             <div className="type" key={index}>
-              <img src={`./images/Types/type${e.name}.png`} alt={e.name} width="50px" height="50px" />
+              <img src={`../images/Types/type${e.name}.png`} alt={e.name} width="50px" height="50px" />
               <p style={{ textTransform: "capitalize" }}> {e.name} </p>
             </div>
           );
